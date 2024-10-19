ec56cb7df4387d1a279e608d4356b5105c62bb13
Thanh Nhan <nhanlam115@gmail.com>
2024-10-19 18:54:07 +0700

    thanh nhan

---
+++ src/layouts/Header.tsx
@@ -0,0 +1,103 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="wrapper">
+        <nav>
+          <ul className="ul">
+            <li className="item">
+              <a href="./Header.tsx">trang chủ</a>
+            </li>
+            <li className="item">
+              <a href="./Header.tsx">sản phẩm</a>
+            </li>
+            <li className="item">
+              <a href="./Header.tsx">giày NT</a>
+            </li>
+            <li className="item">
+              <a href="./Header.tsx">giày FS</a>
+            </li>
+            <li className="item">
+              <a href="./Header.tsx">thương hiệu</a>
+            </li>
+            <li className="item">
+              <a href="./Header.tsx">
+                <span>hot sale</span>
+              </a>
+              <li className="item-child">
+                <a href="./Header.tsx">giày NT</a>
+                <ul className="ul-child">
+                  <li>
+                    <a href="./Header.tsx">giày abc</a>
+                  </li>
+                  <li>
+                    <a href="./Header.tsx">giày vcx</a>
+                  </li>
+                  <li>
+                    <a href="./Header.tsx">giày bjk</a>
+                  </li>
+                  <li>
+                    <a href="./Header.tsx">giày klkllk</a>
+                  </li>
+                  <li>
+                    <a href="./Header.tsx">giày ksss</a>
+                  </li>
+                  <li>
+                    <a href="./Header.tsx">giày ăecx</a>
+                  </li>
+                  <li>
+                    <a href="./Header.tsx">giày bcnms</a>
+                  </li>
+                  <li>
+                    <a href="./Header.tsx">giày 234</a>
+                  </li>
+                </ul>
+              </li>
+              <li className="item-child">
+                <a href="./Header.tsx">giày FT</a>
+                <ul className="ul-child">
+                  <li>
+                    <a href="./Header.tsx">324324</a>
+                  </li>
+                  <li>
+                    <a href="./Header.tsx">dfsdfsdf</a>
+                  </li>
+                  <li>
+                    <a href="./Header.tsx">567567</a>
+                  </li>
+                  <li>
+                    <a href="./Header.tsx">vbnvnv</a>
+                  </li>
+                  <li>
+                    <a href="./Header.tsx">ưqewqewqe</a>
+                  </li>
+                </ul>
+              </li>
+            </li>
+            <li className="item">
+              <a href="./Header.tsx">phụ kiện</a>
+            </li>
+            <li className="item">
+              <a href="./Header.tsx">tin tức giày</a>
+            </li>
+            <li className="item">
+              <a href="./Header.tsx">khách hàng</a>
+            </li>
+            <li className="item">
+              <a href="./Header.tsx">cửa hàng</a>
+            </li>
+            <li className="item">
+              <a href="./Header.tsx">tuyển dụng</a>
+            </li>
+            <li className="item">
+              <a href="./Header.tsx">liên hệ</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
